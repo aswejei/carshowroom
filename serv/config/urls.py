9cccdb6782e71833b865af7986d27a239829bc06
aswejei <nikitaklinchuk@gmail.com>
2022-08-31 17:14:56 +0300

    created jwt obtain/refresh endpoints, added few app endpoints with routers, connected swagger, enabled django admin endpoint

--- serv/config/urls.py
+++ serv/config/urls.py
@@ -1,4 +1,4 @@
-"""autoshop1 URL Configuration
+"""URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,42 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+
+from rest_framework.routers import DefaultRouter
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from supplier.views import CarViewSet, CarPriceRelationSupplierViewSet, SupplierViewSet
+
+router = DefaultRouter()
+router.register(r'cars', CarViewSet)
+router.register(r'suppliers_carprices', CarPriceRelationSupplierViewSet)
+router.register(r'suppliers', SupplierViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CarShowroomAPI",
+        default_version='v1',
+        description="Test description",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    #   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    #    path('__debug__/', include('debug_toolbar.urls')),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
