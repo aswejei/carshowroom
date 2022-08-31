79cd801fa3e105e9883705036e3fd70e3e8889b4
aswejei <nikitaklinchuk@gmail.com>
2022-08-31 17:13:10 +0300

    created views for carshowroom app

--- serv/carshowroom/views.py
+++ serv/carshowroom/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import Response
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from carshowroom.models import CarShowroom, CarPriceRelationShowroom
+from carshowroom.serializers import CarShowroomSerializer, CarPriceRelationShowroomSerializer
+from core.mixins import SafeDeleteModelMixin
+
+
+class CarShowroomViewSet(SafeDeleteModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin,
+                         mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         GenericViewSet):
+    queryset = CarShowroom.objects.filter(is_active=True)
+    serializer_class = CarShowroomSerializer
+
+
+class CarPriceRelationShowroomViewSet(SafeDeleteModelMixin,
+                                      mixins.ListModelMixin,
+                                      mixins.RetrieveModelMixin,
+                                      mixins.CreateModelMixin,
+                                      mixins.UpdateModelMixin,
+                                      GenericViewSet):
+    queryset = CarPriceRelationShowroom.objects.filter(is_active=True)
+    serializer_class = CarPriceRelationShowroomSerializer
