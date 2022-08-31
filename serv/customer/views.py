42569369d129cf85bd0a5182c8033e4923eefae4
aswejei <nikitaklinchuk@gmail.com>
2022-08-31 17:12:26 +0300

    created views for customer and supplier apps

--- serv/customer/views.py
+++ serv/customer/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from core.mixins import SafeDeleteModelMixin
+from customer.models import Customer
+from customer.serializers import CustomerSerializer
+
+
+class CustomerViewSet(SafeDeleteModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.CreateModelMixin,
+                      GenericViewSet):
+    queryset = Customer.objects.filter(is_active=True)
+    serializer_class = CustomerSerializer
--- serv/supplier/views.py
+++ serv/supplier/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from supplier.models import Car, CarPriceRelationSupplier, Supplier
+from supplier.serializers import CarSerializer, CarPriceRelationSupplierSerializer, SupplierSerializer
+from core.mixins import SafeDeleteModelMixin
+
+
+class CarViewSet(mixins.ListModelMixin,
+                 mixins.RetrieveModelMixin,
+                 mixins.CreateModelMixin,
+                 mixins.UpdateModelMixin,
+                 SafeDeleteModelMixin,
+                 GenericViewSet):
+    queryset = Car.objects.filter(is_active=True)
+    serializer_class = CarSerializer
+
+
+class CarPriceRelationSupplierViewSet(SafeDeleteModelMixin,
+                                      mixins.ListModelMixin,
+                                      mixins.RetrieveModelMixin,
+                                      mixins.CreateModelMixin,
+                                      mixins.UpdateModelMixin,
+                                      GenericViewSet):
+    queryset = CarPriceRelationSupplier.objects.filter(is_active=True)
+    serializer_class = CarPriceRelationSupplierSerializer
+
+
+class SupplierViewSet(SafeDeleteModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.CreateModelMixin,
+                      mixins.UpdateModelMixin,
+                      GenericViewSet):
+    queryset = Supplier.objects.filter(is_active=True)
+    serializer_class = SupplierSerializer
