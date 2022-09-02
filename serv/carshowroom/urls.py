8abacd509ba14ccb6c8297d2261cee654595de88
aswejei <nikitaklinchuk@gmail.com>
2022-09-02 14:06:46 +0300

    added urls for carshowroom application

---
+++ serv/carshowroom/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import SimpleRouter
+
+from carshowroom.views import CarShowroomViewSet, CarPriceRelationShowroomViewSet
+
+router = SimpleRouter()
+router.register('', CarShowroomViewSet)
+router.register('prices', CarPriceRelationShowroomViewSet)
+
+urlpatterns = router.urls
