971f07a05aa2d1a1d8253fa2950c577ed2484141
aswejei <nikitaklinchuk@gmail.com>
2022-09-02 15:20:57 +0300

    added rest_framework_simplejwt to INSTALLED_APPS

--- serv/config/settings.py
+++ serv/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_countries',
     'rest_framework',
+    'rest_framework_simplejwt',
     'carshowroom',
     'customer',
     'supplier',
