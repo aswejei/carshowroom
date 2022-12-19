88001765e54763b29ab0031abd186b3f91167ec4
aswejei <nikitaklinchuk@gmail.com>
2022-12-19 16:33:54 +0300

    modified serv launch script

--- serv/docker-entrypoint.sh
+++ serv/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-python3 ./serv/manage.py makemigrations && python3 ./serv/manage.py migrate && python3 ./serv/manage.py runserver 0.0.0.0:8000
-
+python3 ./serv/manage.py makemigrations && python3 ./serv/manage.py migrate && ./serv/manage.py collectstatic --no-input && gunicorn --chdir serv --bind 0.0.0.0:8000 config.wsgi:application
+#&& python3 ./serv/manage.py runserver 0.0.0.0:8000
+#&& gunicorn --chdir serv --bind 0.0.0.0:8000 config.wsgi:application
